3754e68f3acead72236abdaadd8fe1214d23c8cb
Paul  Vivar <Apvivar92@gmail.com>
2022-03-01 00:04:20 -0800

    fixed start menu

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 //start screen for player to read instructions and begin.
 function startGame() {
+    // Add toggle function into startGame function to toggle between start screen to game
+    // False = start-Screen
+    // true = game
+    this.toggleDisplay('start-screen', false);
+    this.toggleDisplay('myCanvas', true);
     let startGameDiv = document.getElementById("start");
-    let gameCanvas
+    let gameCanvas = document.getElementById("myCanvas");
+    let gameOver = document.getElementById("game-over");
+    startGameDiv.style.display = "none";
+    gameCanvas.style.display = "block";
+    gameOver.style.display = "none";
+    startGame();
+};
+
+// Game over screen for when player fails
+function gameOver() {
+    let startGameDiv = document.getElementById("start");
+    let gameCanvas = document.getElementById("myCanvas");
+    let gameOver = document.getElementById("game-over");
+    startGameDiv.style.display = "none";
+    gameCanvas.style.display = "none";
+    gameOver.style.display = "block";
+};
+
+// Make the screen swap between the start screen to the game when start game button is pressed
+function toggleDisplay(id,toggle) {
+    let element = document.getElementById(id);
+    let display = ( toggle ) ? 'block' : 'none';
+    element.style.display = display;
 }
---
+++ game.js
@@ -0,0 +1,32 @@
+// Need to select canva element to draw onto
+const canvas = document.getElementById(#myCanvas);
+const context = canvas.getContext('2d');
+
+canvas
+
+// create user ship with a class then with constructors, define Player's properties.
+class Player {
+    constructor() {
+        // players position
+        this.position = {
+            x: 200,
+            y: 200
+        }
+        // Player's movement Speed
+        this.velocity = {
+            x:0
+        }
+        // Players sprite image
+        // this.image = 
+        this.width = 23
+        this. height = 23
+    }
+    
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
