9e7120df2660a3baf362a2448ec41373790b4766
Paul  Vivar <Apvivar92@gmail.com>
2022-03-03 11:50:46 -0800

    Added PewPew to user ship

--- userShip.js
+++ userShip.js
@@ -46,7 +46,33 @@ class Player {
     }
 }
 
+// User ship projectiles
+class Projectile {
+    constructor({position, velocity}){
+        this.position = position
+        this. velocity = velocity
+
+        this.radius = 5
+    }
+
+    draw() {
+        context.beginPath()
+        // cite https://www.javascripttutorial.net/web-apis/javascript-arc/#:~:text=Introduction%20to%20the%20%EE%80%80JavaScript%20arc%EE%80%81%20%28%29%20method.%20The,draws%20a%20circular%20%EE%80%80arc%EE%80%81%20centered%20at%20%28x%2Cy%20
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        context.fillStyle = 'red'
+        context.fill()
+        context.closePath()
+    }
+    // update animations
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const player = new Player()
+const projectiles = []
 player.draw()
 
 // image won't load due to being called once.
@@ -57,6 +83,9 @@ function animate() {
     context.fillStyle = 'black'
     context.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    projectiles.forEach((projectile) => {
+        projectile.update()
+    })
 }
 
 animate()
@@ -74,6 +103,19 @@ addEventListener('keydown', ({key}) => {
             break
         case ' ':
             console.log('FIRE!')
+            projectiles.push(
+                new Projectile({
+                    position: {
+                        x: player.position.x + player.width /2,
+                        y:player.position.y
+                    },
+                    velocity: {
+                        x: 0,
+                        y: -7
+                    }
+                })
+            )
+            break
     }
 })
 
